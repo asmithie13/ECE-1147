343b2f90197b4d0f8ca06fcaf682009ea23534c7
abbymay1300 <amaier1503@gmail.com>
2024-11-29 21:14:39 -0500

    experiementing with image processing models

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -90,3 +90,58 @@ for ind in range(gc_dev_data.shape[0]):
     gc_dev_dict.update({imInd: [img,
                                 gc_dev_data.iat[ind, 3],
                                 gc_dev_data.iat[ind, 4]]})
+
+from transformers import BlipProcessor, BlipForConditionalGeneration
+
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+
+imInd = gc_dev_data.iat[10, 0]
+imName = str(imInd) + ".jpg"
+imPath = os.path.join(imgIn, "gun_control/", imName)
+img = Image.open(imPath).convert("RGB")
+inputs = processor(img, return_tensors="pt")
+out = model.generate(**inputs)
+print(processor.decode(out[0], skip_special_tokens=True))
+
+'''
+BLIP can kind of describe an image. very basic, kind of wrong
+
+'''
+
+from transformers import AutoProcessor, CLIPModel
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+imInd = gc_dev_data.iat[5, 0]
+imName = str(imInd) + ".jpg"
+imPath = os.path.join(imgIn, "gun_control/", imName)
+img = Image.open(imPath).convert("RGB")
+
+inputs = processor(
+    text=["a photo against gun control", "a photo supporting gun control"], images=img, return_tensors="pt", padding=True
+)
+''' 
+IDEA:
+use CLIP to ask if tweet caption matches tweet image
+use CLIP to ask if image seems to support or oppose gun control
+
+'''
+
+outputs = model(**inputs)
+logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
+probs = logits_per_image.softmax(dim=1) 
+print(probs)
+
+'''
+easyOCR extracts text from images. use with CLIP?
+'''
+
+import easyocr
+
+imInd = gc_dev_data.iat[15, 0]
+imName = str(imInd) + ".jpg"
+imPath = os.path.join(imgIn, "gun_control/", imName)
+reader = easyocr.Reader(['en'])
+result = reader.readtext(imPath)
